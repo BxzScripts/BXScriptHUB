aa42d037a5d499f3369359d330a53883f7394fb7
BxzScripts <bhernardinho05@gmail.com>
2026-04-26 20:27:23 -0300

    Implement Aim module with start and stop functions

--- BxScript/modules/visual/aim.lua
+++ BxScript/modules/visual/aim.lua
@@ -1,1 +1,27 @@
+local Aim = {}
 
+local running = false
+
+function Aim:Start(loadModule)
+	if running then return end
+	running = true
+
+	local Config = loadModule("modules/visual/config.lua")
+	local UIController = loadModule("modules/visual/ui_controller.lua")
+	local TargetDetector = loadModule("modules/visual/target_detector.lua")
+
+	local ui = UIController.new()
+	local detector = TargetDetector.new(ui)
+
+	game:GetService("RunService").RenderStepped:Connect(function()
+		if running then
+			detector:Update(Config.FOV.Radius)
+		end
+	end)
+end
+
+function Aim:Stop()
+	running = false
+end
+
+return Aim
